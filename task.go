76407d21d99aaffef4f37160615fe360332e5535
xeodou <xeodou@gmail.com>
2015-10-22 13:03:55 +0800

    Change to interface.

--- task.go
+++ task.go
@@ -28,34 +28,35 @@ import (
 // 	tm.Tasks = append(tm.Tasks, t...)
 // }
 
-type success func(buf []byte)
-type failure func(err string)
-
-type Task struct {
-	Url     string
-	Success success
-	Failure failure
+type TaskInterface interface {
+	Success(buf []byte)
+	Failure(err error)
 }
 
-func NewTask(url string, suc success, fail failure) *Task {
-	return &Task{url, suc, fail}
+type Task struct {
+	Url      string
+	Listener TaskInterface
+}
+
+func NewTask(url string, listener TaskInterface) *Task {
+	return &Task{url, listener}
 }
 
 type response struct {
 	data chan []byte
-	err  chan string
+	err  chan error
 }
 
 func (t *Task) create() *response {
-	res := &response{make(chan []byte), make(chan string)}
+	res := &response{make(chan []byte), make(chan error)}
 	go func() {
 		for {
 			select {
 			case err := <-res.err:
-				t.Failure(err)
+				t.Listener.Failure(err)
 				break
 			case data := <-res.data:
-				t.Success(data)
+				t.Listener.Success(data)
 			}
 		}
 	}()
@@ -67,11 +68,11 @@ func (t *Task) Runtask() {
 	go func() {
 		res, err := http.Get(t.Url)
 		if err != nil {
-			r.err <- err.Error()
+			r.err <- err
 		} else {
 			buf, err := ioutil.ReadAll(res.Body)
 			if err != nil {
-				r.err <- err.Error()
+				r.err <- err
 			} else {
 				r.data <- buf
 			}
