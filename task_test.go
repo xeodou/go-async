dec0910ef0fb2026ea41895f694017393a3c5746
xeodou <xeodou@gmail.com>
2015-10-22 15:09:18 +0800

    Add tests

--- task_test.go
+++ task_test.go
@@ -7,12 +7,42 @@
 
 package task
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
 
 const (
 	test_url = "https://www.google.com"
+	count    = 1
 )
 
+var wg sync.WaitGroup
+var data string
+
+type Tmp struct{}
+
+func (t *Tmp) Success(buf []byte) {
+	data = string(buf)
+	wg.Done()
+}
+
+func (t *Tmp) Failure(err error) {
+	fmt.Printf("%s", err)
+	wg.Done()
+}
+
 func TestTask(t *testing.T) {
+	wg.Add(1)
+
+	task := NewTask(test_url, &Tmp{})
+	task.Runtask()
+
+	wg.Wait()
+
+	if data == "" {
+		t.Errorf("Run async task failed.")
+	}
 
 }
